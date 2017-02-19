ce4da9889e89942c643b0a5e5d3c3b1565d6ee5d
IanH <ian_web1@hubbertz.de>
2017-02-20 00:23:27 +0100

    Created TouchCtrl class and added support for OLED display

---
+++ src/TouchCtrl.cpp
@@ -0,0 +1,131 @@
+/*
+ * TouchCtrl.cpp
+ *
+ *  Created on: 19.02.2017
+ *      Author: ian
+ */
+
+#include "TouchCtrl.h"
+#include "MPR121.h"
+
+TouchCtrl::TouchCtrl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TouchCtrl::~TouchCtrl() {
+	// TODO Auto-generated destructor stub
+}
+
+void TouchCtrl::setup() {
+	    if(!MPR121.begin(MPR121_I2C_ADDRESS)){
+	      Serial.println("error setting up MPR121");
+	      switch(MPR121.getError()){
+	        case NO_ERROR:
+	          Serial.println("no error");
+	          break;
+	        case ADDRESS_UNKNOWN:
+	          Serial.println("incorrect address");
+	          break;
+	        case READBACK_FAIL:
+	          Serial.println("readback failure");
+	          break;
+	        case OVERCURRENT_FLAG:
+	          Serial.println("overcurrent on REXT pin");
+	          break;
+	        case OUT_OF_RANGE:
+	          Serial.println("electrode out of range");
+	          break;
+	        case NOT_INITED:
+	          Serial.println("not initialised");
+	          break;
+	        default:
+	          Serial.println("unknown error");
+	          break;
+	      }
+	    } else {
+	    	Serial.println("MPR121 initialized");
+	    }
+
+	    MPR121.setInterruptPin(14);
+
+	    // this is the touch threshold - setting it low makes it more like a proximity trigger
+	    // default value is 40 for touch
+	    MPR121.setTouchThreshold(10);
+
+	    // this is the release threshold - must ALWAYS be smaller than the touch threshold
+	    // default value is 20 for touch
+	    MPR121.setReleaseThreshold(6);
+
+	    MPR121.setProxMode(PROX0_3);
+
+	    // initial data update
+	    MPR121.updateTouchData();
+}
+
+void TouchCtrl::readRawInputs(){
+    int i;
+
+    if(MPR121.touchStatusChanged()) MPR121.updateTouchData();
+    MPR121.updateBaselineData();
+    MPR121.updateFilteredData();
+
+
+    Serial.print("TOUCH: ");
+    for(i=0; i<13; i++){          // 13 touch values
+      Serial.print(MPR121.getTouchData(i), DEC);
+      if(i<12) Serial.print(" ");
+
+    }
+    Serial.println();
+
+    Serial.print("FDAT: ");
+    for(i=0; i<13; i++){          // 13 filtered values
+      Serial.print(MPR121.getFilteredData(i), DEC);
+      if(i<12) Serial.print(" ");
+    }
+    Serial.println();
+
+    Serial.print("BVAL: ");
+    for(i=0; i<13; i++){          // 13 baseline values
+      Serial.print(MPR121.getBaselineData(i), DEC);
+      if(i<12) Serial.print(" ");
+    }
+    Serial.println();
+
+    Serial.print("DIFF: ");
+    for(i=0; i<13; i++){          // 13 value pairs
+      Serial.print(MPR121.getBaselineData(i)-MPR121.getFilteredData(i), DEC);
+      if(i<12) Serial.print(" ");
+    }
+    Serial.println();
+
+}
+
+void TouchCtrl::drawFrame(OLEDDisplay& display, OLEDDisplayUiState& state, int16_t x, int16_t y) {
+	static uint32_t next_read = 0;
+	if (millis() > next_read) {
+		readRawInputs();
+		next_read = millis() + 250;
+	}
+
+	display.setFont(ArialMT_Plain_10);
+	bool blink = (millis() >> 8) % 2;
+
+	for (uint_fast8_t i=0;i<=3;i++) {
+		if (blink || !MPR121.getTouchData(i)) {
+			String color(i);
+			color.concat(':');
+			display.drawString(0 + x, 16 + y + (i * 12), color);
+		}
+		int touch_diff = 2* (MPR121.getBaselineData(i)-MPR121.getFilteredData(i));
+		if (touch_diff > 50)  touch_diff = 50;
+		if (touch_diff < -50) touch_diff = -50;
+	    display.drawProgressBar(25+x,16+y+(i*12),100,10,50+touch_diff);
+	}
+}
+
+void TouchCtrl::drawOverlay(OLEDDisplay& display, OLEDDisplayUiState& state, uint8_t idx) {
+	display.setFont(ArialMT_Plain_16);
+	display.drawString(25,0,"Touch Ctrl");
+}
---
+++ src/TouchCtrl.h
@@ -0,0 +1,33 @@
+/*
+ * TouchCtrl.h
+ *
+ *  Created on: 19.02.2017
+ *      Author: ian
+ */
+
+#ifndef SRC_TOUCHCTRL_H_
+#define SRC_TOUCHCTRL_H_
+
+#include "OLEDFrame.h"
+#include "OLEDStatusIndicator.h"
+
+#define MPR121_I2C_ADDRESS 0x5A // 0x5A - 0x5D
+
+class TouchCtrl: public OLEDFrame, OLEDStatusIndicator {
+public:
+	TouchCtrl();
+	virtual ~TouchCtrl();
+	void setup();
+
+	// OLEDFrame
+	virtual void drawFrame(OLEDDisplay& display,  OLEDDisplayUiState& state, int16_t x, int16_t y) override;
+
+	// OLEDOverlay
+	virtual void drawOverlay(OLEDDisplay& display,  OLEDDisplayUiState& state, uint8_t idx) override;
+
+private:
+	void readRawInputs();
+
+};
+
+#endif /* SRC_TOUCHCTRL_H_ */
